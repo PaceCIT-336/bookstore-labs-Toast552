b4d466211e95263b5fe239ef37103e0910682911
G <113320361+Toast552@users.noreply.github.com>
2023-02-27 20:36:08 -0500

    xxxxxxxxxx
    *prays it's in the right repository*

--- index.php
+++ index.php
@@ -9,6 +9,47 @@
     <?php session_start();
     include_once("cart.php"); ?>
 </head>
+<?php
+// Define a constant for the base directory of the website
+define('BASE_DIR', __DIR__);
+
+// Include the books.php file
+require_once(BASE_DIR . '/books.php');
+
+// Check if the $books array is empty or not
+if (empty($books)) {
+    echo 'No books found.';
+    exit;
+}
+
+// Loop through the books array to output the same thing for each book
+foreach ($books as $book) {
+    // Check if all the required properties exist for each book object
+    if (!isset($book->title, $book->image_location, $book->price)) {
+        continue; // Skip this book if any of the required properties are missing
+    }
+
+    // Check if the image file exists for each book
+    $image_path = BASE_DIR . '/' . $book->image_location;
+    if (!file_exists($image_path)) {
+        $image_path = 'https://via.placeholder.com/300x400?text=No+Image'; // Use a placeholder image if the image file is missing
+    }
+
+    // Output the book section element
+    ?>
+    <section class="book">
+        <img src="<?php echo $image_path ?>" alt="<?php echo $book->title ?>" />
+        <h2><?php echo $book->title ?></h2>
+        <?php if (isset($book->author)): ?>
+            <h3><?php echo $book->author ?></h3>
+        <?php endif; ?>
+        <p><?php echo $book->blurb ?></p>
+        <p class="price">$<?php echo $book->price ?></p>
+        <button>Add to Cart</button>
+    </section>
+    <?php
+}
+?>
 
 <body>
     <header>
