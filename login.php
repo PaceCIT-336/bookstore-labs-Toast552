580b9b0bc095fdf0e507037ca005bc0f0054942f
Geoffrey <MLGSTUN@gmail.com>
2023-03-29 17:04:06 -0400

    xxxx

--- login.php
+++ login.php
@@ -1,31 +1,49 @@
 <?php
+require_once 'login.php';
+require_once 'Books.php';
+
 try {
-    // Connection parameters
-    $host = 'localhost';
-    $dbname = 'my_database';
-    $username = 'my_username';
-    $password = 'my_password';
-
-    // Create a PDO instance
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-
-    // Prepare a statement
-    $stmt = $pdo->prepare('SELECT * FROM my_table WHERE id = :id');
-
-    // Bind a value to a parameter
-    $id = 1;
-    $stmt->bindParam(':id', $id);
-
-    // Execute the statement
-    $stmt->execute();
-
-    // Fetch the results
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
-    // Do something with the results
-    
-} catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
-} catch (Exception $e) {
-    echo 'An error occurred: ' . $e->getMessage();
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
+
+$query = "SELECT * FROM books";
+$result = $pdo->query($query);
+
+$books = array();
+
+while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    $id = htmlspecialchars($row['id']);
+    $title = htmlspecialchars($row['title']);
+    $author = htmlspecialchars($row['author']);
+    $description = htmlspecialchars($row['description']);
+    $imagePath = htmlspecialchars($row['image_path']);
+    $price = htmlspecialchars($row['price']);
+    
+    $book = new Book($id, $title, $author, $description, $imagePath, $price);
+    
+    $books[$id] = $book;
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Bookstore</title>
+</head>
+<body>
+	<h1>My Bookstore</h1>
+	<?php foreach ($books as $book) { ?>
+		<div>
+			<h2><?php echo $book->getTitle(); ?></h2>
+			<p>Author: <?php echo $book->getAuthor(); ?></p>
+			<p>Description: <?php echo $book->getDescription(); ?></p>
+			<p>Price: <?php echo $book->getPrice(); ?></p>
+			<img src="<?php echo $book->getImagePath(); ?>" alt="<?php echo $book->getTitle(); ?>">
+		</div>
+	<?php } ?>
+</body>
+</html>
