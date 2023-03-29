5df8de7e02c557035e7a406b7e674650b4d74b64
Geoffrey <MLGSTUN@gmail.com>
2023-03-29 17:16:02 -0400

    xxxxxxxxxx

--- login.php
+++ login.php
@@ -1,6 +1,5 @@
 <?php
 require_once 'login.php';
-require_once 'Books.php';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
@@ -9,41 +8,32 @@ try {
     echo "Connection failed: " . $e->getMessage();
 }
 
-$query = "SELECT * FROM books";
-$result = $pdo->query($query);
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
 
-$books = array();
+    // Retrieve book title from database
+    $query_title = "SELECT title FROM books WHERE id = :id";
+    $stmt_title = $pdo->prepare($query_title);
+    $stmt_title->execute(['id' => $id]);
+    $row_title = $stmt_title->fetch(PDO::FETCH_ASSOC);
+    $title = htmlspecialchars($row_title['title']);
 
-while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-    $id = htmlspecialchars($row['id']);
-    $title = htmlspecialchars($row['title']);
-    $author = htmlspecialchars($row['author']);
-    $description = htmlspecialchars($row['description']);
-    $imagePath = htmlspecialchars($row['image_path']);
-    $price = htmlspecialchars($row['price']);
-    
-    $book = new Book($id, $title, $author, $description, $imagePath, $price);
-    
-    $books[$id] = $book;
+    // Retrieve reviews from database
+    $query_reviews = "SELECT rating, review FROM reviews WHERE book_id = :id";
+    $stmt_reviews = $pdo->prepare($query_reviews);
+    $stmt_reviews->execute(['id' => $id]);
+
+    echo "<h3>Reviews for $title</h3>";
+
+    while($row = $stmt_reviews->fetch(PDO::FETCH_ASSOC)) {
+        $rating = htmlspecialchars($row['rating']);
+        $review = htmlspecialchars($row['review']);
+
+        echo "<p>Rating: $rating</p>";
+        echo "<p>Review: $review</p>";
+        echo "<br>";
+    }
+} else {
+    echo "No book ID provided.";
 }
-
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>My Bookstore</title>
-</head>
-<body>
-	<h1>My Bookstore</h1>
-	<?php foreach ($books as $book) { ?>
-		<div>
-			<h2><?php echo $book->getTitle(); ?></h2>
-			<p>Author: <?php echo $book->getAuthor(); ?></p>
-			<p>Description: <?php echo $book->getDescription(); ?></p>
-			<p>Price: <?php echo $book->getPrice(); ?></p>
-			<img src="<?php echo $book->getImagePath(); ?>" alt="<?php echo $book->getTitle(); ?>">
-		</div>
-	<?php } ?>
-</body>
-</html>
