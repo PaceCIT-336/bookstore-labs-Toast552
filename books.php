fefa7a8020c3b45314c32d0bc290046a2e3b39e2
Geoffrey <MLGSTUN@gmail.com>
2023-04-07 12:23:33 -0400

    xxxxxxxxxxxxxxxxxxxxxxxxx

--- books.php
+++ books.php
@@ -1,31 +1,20 @@
 <?php
 require_once 'login.php';
 
-$host = gethostname();
-$dbname = 'my_database';
-$username = 'my_username';
-$password = 'my_password';
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
-
 $query = "SELECT * FROM books";
 $result = $pdo->query($query);
 
-while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+$books = array();
+
+while ($row = $result->fetch()) {
     $id = htmlspecialchars($row['id']);
     $title = htmlspecialchars($row['title']);
     $author = htmlspecialchars($row['author']);
+    $description = htmlspecialchars($row['description']);
+    $image_path = htmlspecialchars($row['image_path']);
     $price = htmlspecialchars($row['price']);
-    
-    echo "<div>";
-    echo "<h2>$title</h2>";
-    echo "<p>Author: $author</p>";
-    echo "<p>Price: $price</p>";
-    echo "</div>";
+
+    $book = new Book($id, $title, $author, $description, $image_path, $price);
+    $books[$id] = $book;
 }
 ?>
--- login.php
+++ login.php
@@ -1,31 +1,12 @@
 <?php
+$data = 'bookstore';
+$user = 'webapp'; //this is the user you created for lab 3 step 6
+$pass = ''; //the password you set in lab 3 step 6
+
 try {
-    // Connection parameters
-    $host = 'localhost';
-    $dbname = 'my_database';
-    $username = 'my_username';
-    $password = 'my_password';
-
-    // Create a PDO instance
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-
-    // Prepare a statement
-    $stmt = $pdo->prepare('SELECT * FROM my_table WHERE id = :id');
-
-    // Bind a value to a parameter
-    $id = 1;
-    $stmt->bindParam(':id', $id);
-
-    // Execute the statement
-    $stmt->execute();
-
-    // Fetch the results
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
-    // Do something with the results
-    
-} catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
-} catch (Exception $e) {
-    echo 'An error occurred: ' . $e->getMessage();
+    $pdo = new PDO("mysql:host=localhost;dbname=$data", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("ERROR: Could not connect. " . $e->getMessage());
 }
+?>
