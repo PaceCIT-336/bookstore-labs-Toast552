80e657a7fbbbb4e62911d32902ec3d37d55bd2e1
Geoffrey <MLGSTUN@gmail.com>
2023-04-12 18:25:12 -0400

    xxxxxxxxxxxxxxxxxx

--- customers.sql
+++ customers.sql
@@ -1,18 +1,19 @@
 CREATE TABLE customers (
     CustomerID INT AUTO_INCREMENT PRIMARY KEY,
-    FirstName VARCHAR(255),
-    LastName VARCHAR(255),
-    Address VARCHAR(255),
-    City VARCHAR(255),
-    State VARCHAR(255),
-    Zip VARCHAR(10),
-    PhoneNumber VARCHAR(20),
-    Email VARCHAR(255)
+    FirstName VARCHAR(255) NOT NULL,
+    LastName VARCHAR(255) NOT NULL,
+    Address VARCHAR(255) NOT NULL,
+    City VARCHAR(255) NOT NULL,
+    State CHAR(2) NOT NULL,
+    Zip VARCHAR(10) NOT NULL,
+    PhoneNumber VARCHAR(20) NOT NULL,
+    Email VARCHAR(255) NOT NULL,
+    Password VARCHAR(255) NOT NULL
 );
 
-INSERT INTO customers (FirstName, LastName, Address, City, State, Zip, PhoneNumber, Email)
+INSERT INTO customers (FirstName, LastName, Address, City, State, Zip, PhoneNumber, Email, Password)
 VALUES
-    ('John', 'Doe', '123 Main St', 'Anytown', 'CA', '12345', '555-555-1212', 'johndoe@example.com'),
-    ('Jane', 'Smith', '456 Elm St', 'Othertown', 'NY', '67890', '555-555-2121', 'janesmith@example.com');
+    ('John', 'Doe', '123 Main St', 'Anytown', 'CA', '12345', '555-555-1212', 'johndoe@example.com', 'myp@ssword1'),
+    ('Jane', 'Smith', '456 Elm St', 'Othertown', 'NY', '67890', '555-555-2121', 'janesmith@example.com', 'secureP@ss123');
 
-ALTER TABLE Customers ADD Password VARCHAR(255);
+ALTER TABLE customers MODIFY Email VARCHAR(255) UNIQUE;
