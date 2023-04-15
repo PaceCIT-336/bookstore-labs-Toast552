9841836510a436c7ea0653359ef6a3b7dbd6d79b
Geoffrey <MLGSTUN@gmail.com>
2023-04-15 16:12:41 -0400

    xxxxxxxxxxxxx

--- login.php
+++ login.php
@@ -10,3 +10,20 @@ try {
     die("ERROR: Could not connect. " . $e->getMessage());
 }
 ?>
+
+<script>
+  function validateForm() {
+    // Perform form validation using JavaScript
+    // Add your custom validation logic here
+    // Return true if validation passes, false otherwise
+    return true;
+  }
+  
+  // Example: Attach the form validation function to the form's submit event
+  var form = document.querySelector('form');
+  form.addEventListener('submit', function(event) {
+    if (!validateForm()) {
+      event.preventDefault(); // Prevent form submission if validation fails
+    }
+  });
+</script>
