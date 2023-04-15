c4e5dc49418d78db212bf7552be1e725b1f88afc
Geoffrey <MLGSTUN@gmail.com>
2023-04-15 16:14:04 -0400

    xxxxxxxxxxxxxxxxxxxxx

--- purchases.sql
+++ purchases.sql
@@ -3,31 +3,45 @@ require_once 'login.php';
 
 // Assuming $pdo is properly initialized with a PDO object
 
-// Create Purchases table with proper data types and constraints
-$query = "CREATE TABLE IF NOT EXISTS Purchases (
-            PurchaseID INT UNSIGNED NOT NULL AUTO_INCREMENT,
-            BookID INT UNSIGNED NOT NULL,
-            CustomerID INT UNSIGNED NOT NULL,
-            OrderDate DATE NOT NULL,
-            PRIMARY KEY (PurchaseID),
-            FOREIGN KEY (BookID) REFERENCES Books(BookID),
-            FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
-          ) ENGINE=InnoDB;";
-$pdo->exec($query);
+try {
+  // Start a transaction
+  $pdo->beginTransaction();
 
-// Insert data into Purchases table using prepared statements for security
-$query = "INSERT INTO Purchases (BookID, CustomerID, OrderDate) VALUES (?, ?, ?), (?, ?, ?)";
-$stmt = $pdo->prepare($query);
+  // Create Purchases table with proper data types and constraints
+  $query = "CREATE TABLE IF NOT EXISTS Purchases (
+              PurchaseID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+              BookID INT UNSIGNED NOT NULL,
+              CustomerID INT UNSIGNED NOT NULL,
+              OrderDate DATE NOT NULL,
+              PRIMARY KEY (PurchaseID),
+              FOREIGN KEY (BookID) REFERENCES Books(BookID),
+              FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+            ) ENGINE=InnoDB;";
+  $pdo->exec($query);
 
-// Bind parameter values to prepared statement
-$stmt->bindValue(1, 1, PDO::PARAM_INT);
-$stmt->bindValue(2, 1, PDO::PARAM_INT);
-$stmt->bindValue(3, '2022-01-01', PDO::PARAM_STR);
-$stmt->bindValue(4, 2, PDO::PARAM_INT);
-$stmt->bindValue(5, 2, PDO::PARAM_INT);
-$stmt->bindValue(6, '2022-01-02', PDO::PARAM_STR);
+  // Insert data into Purchases table using prepared statements for security
+  $query = "INSERT INTO Purchases (BookID, CustomerID, OrderDate) VALUES (?, ?, ?), (?, ?, ?)";
+  $stmt = $pdo->prepare($query);
 
-// Execute prepared statement
-$stmt->execute();
+  // Bind parameter values to prepared statement
+  $stmt->bindValue(1, 1, PDO::PARAM_INT);
+  $stmt->bindValue(2, 1, PDO::PARAM_INT);
+  $stmt->bindValue(3, '2022-01-01', PDO::PARAM_STR);
+  $stmt->bindValue(4, 2, PDO::PARAM_INT);
+  $stmt->bindValue(5, 2, PDO::PARAM_INT);
+  $stmt->bindValue(6, '2022-01-02', PDO::PARAM_STR);
+
+  // Execute prepared statement
+  $stmt->execute();
+
+  // Commit the transaction
+  $pdo->commit();
+
+  echo "Purchases table created and data inserted successfully.";
+} catch (PDOException $e) {
+  // Rollback the transaction in case of an error
+  $pdo->rollBack();
+  die("ERROR: " . $e->getMessage());
+}
 
 ?>
