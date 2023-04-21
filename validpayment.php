b7585856ad030bcc3f6f047591de3884549f9e9c
Geoffrey <MLGSTUN@gmail.com>
2023-04-21 19:32:17 -0400

    xxxxxxxxxxxxxxxxx

--- validpayment.php
+++ validpayment.php
@@ -1,91 +1,92 @@
 <?php
-    // PHP code here
+// define variables and set to empty values
+$nameErr = $emailErr = $cardErr = $cvvErr = "";
+$name = $email = $card = $cvv = "";
 
-    // Function to fix string and strip HTML entities
-    function fix_string($string) {
-        // Add your implementation for fixing the string here
-        // For example, you can use htmlentities function:
-        return htmlentities($string);
-    }
+function fix_string($string) {
+    if (get_magic_quotes_gpc()) $string = stripslashes($string);
+    return htmlentities ($string);
+}
 
-    // Function to validate full name
-    function validateFullName($fullName) {
-        if (empty($fullName)) {
-            return "Full Name is required.";
-        }
-        return "";
-    }
+function validate_name($field) {
+    if ($field == "") return "Name is required<br>";
+    return "";
+}
 
-    // Function to validate email
-    function validateEmail($email) {
-        if (empty($email)) {
-            return "Email is required.";
-        } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            return "Email format is incorrect.";
-        }
-        return "";
-    }
+function validate_email($field) {
+    if ($field == "") return "Email is required<br>";
+    else if (!preg_match("/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/", $field))
+        return "Invalid email format<br>";
+    return "";
+}
 
-    // Function to validate credit card number
-    function validateCreditCardNumber($creditCardNumber) {
-        if (empty($creditCardNumber)) {
-            return "Credit Card Number is required.";
-        } else if (!preg_match('/^[0-9]{16}$/', $creditCardNumber)) {
-            return "Credit Card Number must be 16 digits.";
-        }
-        return "";
-    }
+function validate_card($field) {
+    if ($field == "") return "Credit card number is required<br>";
+    else if (!preg_match("/^[0-9]{16}$/", $field))
+        return "Invalid credit card number<br>";
+    return "";
+}
 
-    // Function to validate CVV
-    function validateCVV($cvv) {
-        if (empty($cvv)) {
-            return "CVV is required.";
-        } else if (!preg_match('/^[0-9]{3}$/', $cvv)) {
-            return "CVV must be 3 digits.";
-        }
-        return "";
-    }
+function validate_cvv($field) {
+    if ($field == "") return "CVV is required<br>";
+    else if (!preg_match("/^[0-9]{3}$/", $field))
+        return "Invalid CVV number<br>";
+    return "";
+}
 
-    // Check if form is submitted
-    if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["fullName"])) {
-        // Retrieve form data
-        $fullName = fix_string($_POST["fullName"]);
-        $email = fix_string($_POST["email"]);
-        $creditCardNumber = fix_string($_POST["creditCardNumber"]);
-        $cvv = fix_string($_POST["cvv"]);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = fix_string($_POST["name"]);
+    $email = fix_string($_POST["email"]);
+    $card = fix_string($_POST["card"]);
+    $cvv = fix_string($_POST["cvv"]);
 
-        // Validate each input and store errors in $fail variable
-        $fail = "";
-        $fail .= validateFullName($fullName);
-        $fail .= validateEmail($email);
-        $fail .= validateCreditCardNumber($creditCardNumber);
-        $fail .= validateCVV($cvv);
+    $nameErr .= validate_name($name);
+    $emailErr .= validate_email($email);
+    $cardErr .= validate_card($card);
+    $cvvErr .= validate_cvv($cvv);
 
-        // If there are no errors, process payment information
-        if (empty($fail)) {
-            // Perform processing of payment information
-            // Add your processing logic here
+    $fail = $nameErr.$emailErr.$cardErr.$cvvErr;
+}
 
-            // Debugging: Display success message
-            echo "Payment information submitted successfully!";
-        } else {
-            // Debugging: Display error messages
-            echo "Errors found: " . $fail;
-        }
-    }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Payment Information</title>
+	<title>Payment Information</title>
 </head>
 <body>
-    <h1>Payment Information</h1>
-    <form method="post" action="">
-        <label for="fullName">Full Name:</label><br>
-        <input type="text" id="fullName" name="fullName" required placeholder="Enter Full Name" value="<?php echo isset($fullName) ? $fullName : '' ?>"><br><br>
-        <label for="email">Email:</label><br>
-        <input type="email" id="email" name="email" required placeholder="Enter Email" value="<?php echo isset($email) ? $email : '' ?>"><br><br>
-        <label for="creditCardNumber">Credit Card Number:</label><br>
-        <input type="text" id="creditCardNumber" name="creditCardNumber" required placeholder="Enter Credit Card Number" value="<?php echo isset($creditCardNumber) ? $creditCardNumber : '' ?>"><br
+
+	<h2>Payment Information</h2>
+
+	<?php
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		if ($fail == "") {
+			echo "<p>Thank you for your payment!</p>";
+			echo "<p>Name: " . $name . "</p>";
+			echo "<p>Email: " . $email . "</p>";
+			echo "<p>Credit Card: " . $card . "</p>";
+			echo "<p>CVV: " . $cvv . "</p>";
+		} else {
+			echo "<p>Please correct the following errors:</p>";
+			echo "<ul>" . $fail . "</ul>";
+		}
+	}
+	?>
+
+	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+		<label for="name">Full Name:</label><br>
+		<input type="text" id="name" name="name" value="<?php echo $name;?>" required><br>
+
+		<label for="email">Email:</label><br>
+		<input type="email" id="email" name="email" value="<?php echo $email;?>" required><br>
+
+		<label for="card">Credit Card Number:</label><br>
+		<input type="text" id="card" name="card" value="<?php echo $card;?>" required><br>
+
+		<label for="cvv">CVV:</label><br>
+		<input type="text" id="cvv" name="cvv" value="<?php echo $cvv;?>" required><br>
+
+		<input type="submit" value="Submit">
+	</form>
+
