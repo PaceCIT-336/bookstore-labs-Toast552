e322f087f2d52fe248cee7b5839a7003ef356986
Geoffrey <MLGSTUN@gmail.com>
2023-04-21 18:23:14 -0400

    xxxxxxxxxxxxxxxx

--- books.php
+++ books.php
@@ -1,20 +1,25 @@
 <?php
 require_once 'login.php';
 
-$query = "SELECT * FROM books";
-$result = $pdo->query($query);
+try {
+  $query = "SELECT * FROM books";
+  $result = $pdo->query($query);
 
-$books = array();
+  $books = array();
 
-while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-    $id = htmlspecialchars($row['id'], ENT_QUOTES, 'UTF-8');
-    $title = htmlspecialchars($row['title'], ENT_QUOTES, 'UTF-8');
-    $author = htmlspecialchars($row['author'], ENT_QUOTES, 'UTF-8');
-    $description = htmlspecialchars($row['description'], ENT_QUOTES, 'UTF-8');
-    $image_path = htmlspecialchars($row['image_path'], ENT_QUOTES, 'UTF-8');
-    $price = htmlspecialchars($row['price'], ENT_QUOTES, 'UTF-8');
+  while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+      $id = htmlspecialchars($row['id'], ENT_QUOTES, 'UTF-8');
+      $title = htmlspecialchars($row['title'], ENT_QUOTES, 'UTF-8');
+      $author = htmlspecialchars($row['author'], ENT_QUOTES, 'UTF-8');
+      $description = htmlspecialchars($row['description'], ENT_QUOTES, 'UTF-8');
+      $image_path = htmlspecialchars($row['image_path'], ENT_QUOTES, 'UTF-8');
+      $price = htmlspecialchars($row['price'], ENT_QUOTES, 'UTF-8');
 
-    $book = new Book($id, $title, $author, $description, $image_path, $price);
-    $books[$id] = $book;
+      $book = new Book($id, $title, $author, $description, $image_path, $price);
+      $books[$id] = $book;
+  }
+} catch(PDOException $e) {
+  // Handle database error
+  echo "Error: " . $e->getMessage();
 }
 ?>
